9d89631824585f346405309397f4729015b67cea
TakahashiShoma <shoma.takahashi@acroit.co.jp>
2021-10-05 11:03:05 +0900

    211005Takahashi

--- nyuka.php
+++ nyuka.php
@@ -26,8 +26,10 @@ if (/* ③の処理を書く */){
 }
 
 //⑥データベースへ接続し、接続情報を変数に保存する
+$pdo = new PDO($dsn, $username, $password, $driver_options);
 
 //⑦データベースで使用する文字コードを「UTF8」にする
+$str = mb_convert_encoding($str, "UTF-8", "auto");
 
 //⑧POSTの「books」の値が空か判定する。空の場合はif文の中に入る。
 if(/* ⑧の処理を行う */){
