2734f37199e570c8ed442cffc630967d4b09ea4d
Sou-96 <N_Sohei-_-@outlook.jp>
2021-10-08 14:33:32 +0900

    入荷⑬・⑭

--- nyuka.php
+++ nyuka.php
@@ -80,8 +80,9 @@ function getId($id,$con){
 			 * ⑬SESSIONの「error」にメッセージが設定されているかを判定する。
 			 * 設定されていた場合はif文の中に入る。
 			 */ 
-			if(/* ⑬の処理を書く */){
+			if($_SESSION["error"]){
 				//⑭SESSIONの「error」の中身を表示する。
+				echo $_SESSION["error"];
 			}
 			?>
 			</div>
