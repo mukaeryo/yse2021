642959c739662bf6055d4f98941529fb7d9eb1ab
TakahashiShoma <shoma.takahashi@acroit.co.jp>
2021-10-12 15:35:20 +0900

    211012Takahashi4

--- syukka_kakunin.php
+++ syukka_kakunin.php
@@ -118,7 +118,7 @@ if(isset($_POST['add'])&&$_POST['add'] == "ok"){
 	}
 
 	//㉚SESSIONの「success」に「入荷が完了しました」と設定する。
-	array_push($success, '入荷が完了しました');
+	$_SESSION["success"]="入荷が完了しました";
 	//㉛「header」関数を使用して在庫一覧画面へ遷移する。
 	header("Location: login.php");
 }
